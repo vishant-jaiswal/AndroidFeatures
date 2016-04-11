b18cbec5001335220c6e44524cfadc9ed9ea1528
coderjerry <vishantjaiswal@yahoo.com>
2016-04-11 21:23:14 +0530

    Commit Sample code for FireBaseUi Integration in ListView

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,7 +1,13 @@
 package com.coderchowki.featureapps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list_message = (ListView) findViewById(R.id.list_message);
+        Firebase.setAndroidContext(this);
+
+        Firebase ref = new Firebase("https://woodroom.firebaseio.com/msg");
+
+        FirebaseListAdapter mAdapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class, android.R.layout.two_line_list_item, ref) {
+            @Override
+            protected void populateView(View view, ChatMessage chatMessage, int position) {
+                ((TextView) view.findViewById(android.R.id.text1)).setText(chatMessage.getName());
+                ((TextView) view.findViewById(android.R.id.text2)).setText(chatMessage.getMessage());
+
+            }
+        };
+        list_message.setAdapter(mAdapter);
     }
 }
