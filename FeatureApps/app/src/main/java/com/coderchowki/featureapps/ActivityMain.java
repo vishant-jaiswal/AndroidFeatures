41a538613339be35c574eb08735c7ceb67b3ce63
coderjerry <vishantjaiswal@yahoo.com>
2016-04-12 14:01:04 +0530

    Commit Sample code for FireBaseUi Authentication Integration this commit is only to switch to other branch to get sample code from gradle

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,13 +1,42 @@
 package com.coderchowki.featureapps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityMain extends AppCompatActivity {
+import com.firebase.client.Firebase;
+import com.firebase.ui.auth.core.AuthProviderType;
+import com.firebase.ui.auth.core.FirebaseLoginBaseActivity;
+import com.firebase.ui.auth.core.FirebaseLoginError;
+
+public class ActivityMain extends FirebaseLoginBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // All providers are optional! Remove any you don't want.
+        //setEnabledAuthProvider(AuthProviderType.FACEBOOK);
+        setEnabledAuthProvider(AuthProviderType.TWITTER);
+        //setEnabledAuthProvider(AuthProviderType.GOOGLE);
+        setEnabledAuthProvider(AuthProviderType.PASSWORD);
+    }
+
+    @Override
+    protected Firebase getFirebaseRef() {
+        return null;
+    }
+
+    @Override
+    protected void onFirebaseLoginProviderError(FirebaseLoginError firebaseLoginError) {
+
+    }
+
+    @Override
+    protected void onFirebaseLoginUserError(FirebaseLoginError firebaseLoginError) {
+
     }
 }
