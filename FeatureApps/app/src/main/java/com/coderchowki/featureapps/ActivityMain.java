d574c62170c9440f325d1dacbc823a1fbe208440
coderjerry <vishantjaiswal@yahoo.com>
2016-04-14 03:26:30 +0530

    Commit Sample code for ProgressBar Horizontal

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,13 +1,49 @@
 package com.coderchowki.featureapps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private static final int PROGRESS = 0x1;
+    private static final String TAG = "PRBAR" ;
+
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 100) {
+                    mProgressStatus += 5;
+                    try {
+                        // Sleep for 5 seconds
+                        Thread.sleep(1000);
+                        Log.d(TAG, "sleep success" + mProgressStatus);
+                    } catch (InterruptedException e) {
+                        Log.d(TAG, "sleep failure");
+                    }
+                    mProgress.setProgress(mProgressStatus);
+
+
+                }
+            }
+        }).start();
+
+
+    }
+
+    private int doWork() {
+        return 0;
     }
 }
