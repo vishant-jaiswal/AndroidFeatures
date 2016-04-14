f56636b4cacc346ae6fb10a25ba3e268bcc2e73e
coderjerry <vishantjaiswal@yahoo.com>
2016-04-14 21:05:41 +0530

    Initial commit for recycler view

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -2,12 +2,20 @@ package com.coderchowki.featureapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class ActivityMain extends AppCompatActivity {
+
+    //TODO: 2> create an object of recyclerview
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: 3> get the refernce of an object of recyclerview
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
     }
 }
