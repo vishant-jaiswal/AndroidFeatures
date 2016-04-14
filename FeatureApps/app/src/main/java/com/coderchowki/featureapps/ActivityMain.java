166a4083fc94fda9f61a5d39f73df4e7634d54f7
coderjerry <vishantjaiswal@yahoo.com>
2016-04-14 17:18:23 +0530

    Commiting initial code for SMS read operation and display in TextView

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,13 +1,41 @@
 package com.coderchowki.featureapps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private static final String TAG = "SMSRead";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView view = new TextView(this);
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        Cursor cur = getContentResolver().query(uriSMSURI, null, null, null,null);
+        String sms = "";
+        while (cur.moveToNext()) {
+            sms += "From :" + cur.getString(2) + " : " + cur.getString(11)+"\n";
+            Log.i(TAG,""+cur.getString(1)+":"
+                    +cur.getString(2)+":"
+                    +cur.getString(3)+":"
+                    +cur.getString(4)+":"
+                    +cur.getString(5)+":"
+                    +cur.getString(6)+":"
+                    +cur.getString(7)+":"
+                    +cur.getString(8)+":"
+                    +cur.getString(9)+":"
+                    +cur.getString(10)+":"
+                    +cur.getString(11)
+                    +cur.getString(cur.getColumnIndexOrThrow("body"))+":"
+                    +cur.getString(cur.getColumnIndexOrThrow("address"))+"\n");
+        }
+        view.setText(sms);
+        setContentView(view);
     }
 }
