3617b478eff5baf8316fb8a6cdba9f63b3250297
coderjerry <vishantjaiswal@yahoo.com>
2016-04-26 16:24:05 +0530

    Commiting the sample code for GradeRecord example...StudentFragment is throwing error while adding new student. The error is comming from firebase as permission denied.. Will be fixed in Upcomming Commit

---
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/Constants.java
@@ -0,0 +1,18 @@
+package com.coderchowki.featureapps;
+
+/**
+ * Created by Matt Boutell on 9/18/2015.
+ */
+public class Constants {
+    public static final String PREFS = "PREFS";
+    public static final String UID_KEY = "UID_KEY";
+    public static final String TAG = "GRF";
+    public static final String COURSE_KEY = "COURSE_KEY";
+    public static final String FIREBASE_URL = "http://graderecorder.firebaseio.com";
+    public static final String COURSES_PATH = FIREBASE_URL + "/courses";
+    public static final String OWNERS_PATH = FIREBASE_URL + "/owners";
+    public static final String ASSIGNMENTS_PATH = FIREBASE_URL + "/assignments";
+    public static final String STUDENTS_PATH = FIREBASE_URL + "/students";
+    public static final String GRADE_ENTRIES_PATH = FIREBASE_URL + "/grade_entries";
+    public static final String ROSEFIRE_REGISTRY_TOKEN = "PASTE_YOUR_TOKEN_HERE";
+}
