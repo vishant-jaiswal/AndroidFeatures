bf29fe87433e8325334b8c8a361cfbf6925924d0
coderjerry <vishantjaiswal@yahoo.com>
2016-07-24 13:14:39 +0530

    checkin before making changes to ActivityMain to implement MyLocation class

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,24 +1,27 @@
 package com.coderchowki.featureapps;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+public class ActivityMain extends AppCompatActivity implements LocationListener {
 
-public class ActivityMain extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-    GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private TextView mLatitudeText;
     private TextView mLongitudeText;
+    private TextView mProvider;
+
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,33 +29,23 @@ public class ActivityMain extends AppCompatActivity implements GoogleApiClient.C
         setContentView(R.layout.activity_main);
         mLatitudeText = (TextView) findViewById(R.id.tv_latitude);
         mLongitudeText = (TextView) findViewById(R.id.tv_longitude);
+        mProvider = (TextView) findViewById(R.id.tv_provider);
 
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
+        // check if enabled and if not send user to the GSP settings
+// Better solution would be to display a dialog and suggesting to
+// go to the settings
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
         }
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
+        provider = service.getBestProvider(criteria, false);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -61,24 +54,58 @@ public class ActivityMain extends AppCompatActivity implements GoogleApiClient.C
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            Log.i("LocationCheck","permission not granted");
             return;
         }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLastLocation != null) {
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        Location location = service.getLastKnownLocation(provider);
+        mProvider.setText(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            mLatitudeText.setText("Location not available");
+            mLongitudeText.setText("Location not available");
         }
 
+
+        // Create an instance of GoogleAPIClient.
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Double lat = (Double) (location.getLatitude());
+        Double lng = (Double) (location.getLongitude());
+        mLatitudeText.setText(String.valueOf(lat));
+        mLongitudeText.setText(String.valueOf(lng));
 
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider, Toast.LENGTH_SHORT).show();
 
     }
 }
