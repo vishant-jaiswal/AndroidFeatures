9b74ed3e0ae6db9d37af961eb1ff8035d36a7a3d
coderjerry <vishantjaiswal@yahoo.com>
2016-08-28 14:05:30 +0530

    Example for Custom Dialog Fragment and Alert Dialog Fragment

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -1,13 +1,43 @@
 package com.coderchowki.featureapps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements MyDialogFragment.UserNameListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onFinishUserDialog(String user) {
+        Toast.makeText(this, "Hello, " + user, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view) {
+        // close existing dialog fragments
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment frag = manager.findFragmentByTag("fragment_edit_name");
+        if (frag != null) {
+            manager.beginTransaction().remove(frag).commit();
+        }
+        switch (view.getId()) {
+            case R.id.showCustomFragment:
+                MyDialogFragment editNameDialog = new MyDialogFragment();
+                editNameDialog.show(manager, "fragment_edit_name");
+                break;
+            case R.id.showAlertDialogFragment:
+                MyAlertDialogFragment alertDialogFragment = new MyAlertDialogFragment();
+                alertDialogFragment.show(manager, "fragment_edit_name");
+                break;
+        }
+    }
+
+
 }
---
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/MyAlertDialogFragment.java
@@ -0,0 +1,37 @@
+package com.coderchowki.featureapps;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+/**
+ * Created by Vishant on 28-08-2016.
+ */
+public class MyAlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                // set dialog icon
+                .setIcon(android.R.drawable.stat_notify_error)
+                // set Dialog Title
+                .setTitle("Alert dialog fragment example")
+                // Set Dialog Message
+                .setMessage("This is a message")
+
+                // positive button
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Pressed OK", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                // negative button
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel", Toast.LENGTH_SHORT).show();
+                    }
+                }).create();
+    }
+}
