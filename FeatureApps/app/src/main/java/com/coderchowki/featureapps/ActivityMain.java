126a71d3ce054fef33e5fb0fde800763f966435c
Vishant <vishantjaiswal@yahoo.com>
2016-12-01 12:20:31 +0530

    MultiLanguage Support For multiActivity Application is working by setting locale in one activity itself

--- FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
+++ FeatureApps/app/src/main/java/com/coderchowki/featureapps/ActivityMain.java
@@ -3,18 +3,30 @@ package com.coderchowki.featureapps;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initializeView();
+
+    }
+
+    private void initializeView() {
+        this.setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
     }
 
     @Override
@@ -34,7 +46,7 @@ public class ActivityMain extends AppCompatActivity {
                 config.locale = locale;
                 getBaseContext().getResources().updateConfiguration(config,
                         getBaseContext().getResources().getDisplayMetrics());
-                this.setContentView(R.layout.activity_main);
+                //this.setContentView(R.layout.activity_main);
                 break;
             case R.id.hn:
                 languageToLoad = "hi"; // your language
@@ -44,7 +56,7 @@ public class ActivityMain extends AppCompatActivity {
                 config.locale = locale;
                 getBaseContext().getResources().updateConfiguration(config,
                         getBaseContext().getResources().getDisplayMetrics());
-                this.setContentView(R.layout.activity_main);
+                //this.setContentView(R.layout.activity_main);
                 break;
             case R.id.fr:
                 languageToLoad = "fr"; // your language
@@ -54,7 +66,7 @@ public class ActivityMain extends AppCompatActivity {
                 config.locale = locale;
                 getBaseContext().getResources().updateConfiguration(config,
                         getBaseContext().getResources().getDisplayMetrics());
-                this.setContentView(R.layout.activity_main);
+                //this.setContentView(R.layout.activity_main);
                 break;
             case R.id.de:
                 languageToLoad = "de"; // your language
@@ -64,11 +76,20 @@ public class ActivityMain extends AppCompatActivity {
                 config.locale = locale;
                 getBaseContext().getResources().updateConfiguration(config,
                         getBaseContext().getResources().getDisplayMetrics());
-                this.setContentView(R.layout.activity_main);
+                //this.setContentView(R.layout.activity_main);
                 break;
             default:
                 break;
         }
+        initializeView();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn){
+            Intent intent = new Intent(ActivityMain.this,Main2Activity.class);
+            startActivity(intent);
+        }
     }
 }
